21039369fab42089309e4ec66545bf0961bcfec2
LennyPenny <l.bernhardt@live.de>
2014-09-06 23:46:01 +0200

    maybe build naow

--- premake5.lua
+++ premake5.lua
@@ -1,52 +1,16 @@
-assert(os.getenv("SOURCE_SDK"), "SOURCE_SDK environmental variable not set")
+dofile("../../common.lua")
 
-local SOURCE_SDK = os.getenv("SOURCE_SDK") .. "/mp/src"
+RequireDefaultlibs()
 
+SOLUTION 	"gm_roc"
 
-solution "gmcl_roc_win32"
-	language "C++"
-	location ( os.get() .."-".. _ACTION )
-	targetdir ( "build/" .. os.get() .. "/" )
+targetdir	"bins"
+INCLUDES	"source_sdk"
+INCLUDES 	"gmod_sdk"
+defines		{"NDEBUG"}
 
-	includedirs {
-		"deps",
-		SOURCE_SDK .. "/common",
-		SOURCE_SDK .. "/public/tier0",
-		SOURCE_SDK .. "/public/tier1",
-		SOURCE_SDK .. "/public"
-	}
+WINDOWS()
 
-	libdirs {
-		"deps",
-		SOURCE_SDK .. "/lib/public"
-	}
-
-	links {
-		"tier0",
-		"tier1",
-		"vstdlib",
-		"Shlwapi",
-	}
-	
-	configurations
-	{ 
-		"Release"
-	}
-	
-	configuration "Release"
-		defines { "NDEBUG" }
-		optimize "On"
-
-	configuration "vs*"
-		linkoptions {
-			"/NODEFAULTLIB:libcmt"
-		}
-
-	project "gmcl_roc_win32"
-		defines { "GMMODULE" }
-		kind "SharedLib"
-		
-		files {
-			"src/**.cpp",
-			"src/**.h"
-		}
+PROJECT()
+SOURCE_SDK_LINKS()
+configuration 		"windows"
